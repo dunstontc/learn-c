81035433ad243aa5e1b6113d70651f8b7a54957f
Clay Dunston <dunstontc@gmail.com>
2018-05-03 23:00:36 -0500

    moved input to new folder

--- code/input/input.c
+++ code/input/input.c
@@ -4,13 +4,11 @@ int main()
 {
     int number;
 
-    // printf() dislpays the formatted output
     printf("Enter an integer: ");
 
-    // scanf() reads the formatted input and stores them
     scanf("%d", &number);
 
-    // printf() displays the formatted output
     printf("You entered: %d!\n", number);
+
     return 0;
 }
