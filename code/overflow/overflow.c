981c17e7cb6938b4e1c15bb97846739dd5b1eb58
Clay Dunston <dunstontc@gmail.com>
2018-05-03 23:02:41 -0500

    add overflow

---
+++ code/overflow/overflow.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * Each variable type can only hold so much.
+ * In C when you declare a variable, you're saying
+ * "this is how much space I want to allocate and how I want it stored within the stack".
+ * The short will overflow its available value, go negative, and continue the cycle.
+ * It will never be as big as a long long.
+ */
+int main(void)
+{
+    short a = 0;
+    long long b = 999999999999;
+
+    while(a < b)
+    {
+        printf("%d\n", a);
+        ++a;
+    }
+    return 0;
+}
